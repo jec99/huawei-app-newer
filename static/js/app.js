b8d78bd4a0cef638c1b773092b7091235dcb1d14
Julien Clancy <clancy.julien@gmail.com>
2015-06-29 14:56:38 +0800

    geojson points on the map

--- static/js/app.js
+++ static/js/app.js
@@ -21,9 +21,32 @@ mapApp.factory('sampleData', function ($q, $http) {
 	};
 });
 
-mapApp.controller('MainController', function ($scope, sampleData) {
+mapApp.factory('sampleGeoJSON', function ($q, $http) {
+	var geoJSON;
+	return {
+		getGeoJSON: function () {
+			var deferred = $q.defer();
+			if (!geoJSON) {
+				$http.get('/data_geojson'
+				).success(function (data, status, headers, response) {
+					geoJSON = data;
+					deferred.resolve(data);
+				}).error(function (data, status, headers, response) {
+					deferred.reject(status);
+				});
+			}
+
+			return deferred.promise;
+		}
+	};
+});
+
+mapApp.controller('MainController', function ($scope, sampleData, sampleGeoJSON) {
 	$scope.text = 'Hello this is app';
 	sampleData.getSampleData().then(function (data) {
 		$scope.data = data;
 	});
+	sampleGeoJSON.getGeoJSON().then(function (data) {
+		$scope.geojson = data;
+	})
 });
