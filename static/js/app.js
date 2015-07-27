28197f5d52b74b52bd9cff6e9a891e1a6cd25236
Julien Clancy <clancy.julien@gmail.com>
2015-07-27 20:58:39 +0800

    heatmap functionality, but not displayed right now because it is ugly

--- static/js/app.js
+++ static/js/app.js
@@ -261,13 +261,6 @@ angular.module('mapApp', ['mapApp.factories', 'mapApp.mapController', 'ngMateria
 				.tickFormat(function (e) { return e; })
 		];
 
-		// THESE ARE BUGGY
-		// FIX THEM LATER
-		// what's going on is, the scalar charts are not brushing
-		// the actual extent (/5), but the extent labeled
-		// same with teh subscribed chart actually, i have no idea
-		// how i didn't catch this. i guess we have to pass in
-		// another function...
 		weather_charts = [
 			barChart()
 				.dimension(temperature)
@@ -529,6 +522,7 @@ angular.module('mapApp', ['mapApp.factories', 'mapApp.mapController', 'ngMateria
 			var scaling = d3.event ? semanticZoom(d3.event.scale) : zoomMap.scaleExtent()[0];
 			return 'translate(' + xScatterMap(coords[0]) + ',' + yScatterMap(coords[1]) + ')scale(' + scaling + ')';
 		}
+
 		return pointsOfInterestFactory.get();
 	}).then(function (data) {
 		var g = d3.select(map_elt)
@@ -540,7 +534,7 @@ angular.module('mapApp', ['mapApp.factories', 'mapApp.mapController', 'ngMateria
 		var locations = g.selectAll('circle')
 			.data(data.data)
 			.enter().append('circle')
-			.attr('r', function (d) { return 7 / Math.pow(d.rank, 1 / 2); })
+			.attr('r', function (d) { return 7 / Math.pow(d.rank, 0.5); })
 			.style('fill', '#FF5252')
 			.attr('transform', transform);
 
@@ -558,5 +552,38 @@ angular.module('mapApp', ['mapApp.factories', 'mapApp.mapController', 'ngMateria
 	});
 });
 
+function container () {
+	// use heatmaps to visualize the results of the PageRank algorithm
+	// maybe do that thing where we have the layers stacked in 3d space
 
+	var heatmap = h337.create({
+		radius: 20,
+		blur: 1,
+		maxOpacity: 0.9,
+		minOpacity: 0.3,
+		useLocalExtrema: true,
+		gradient: {
+			0       : '#eee',
+			0.2     : '#eee',
+			0.20001 : '#bbb',
+			0.4     : '#bbb',
+			0.40001 : '#888',
+			0.6     : '#888',
+			0.60001 : '#444',
+			0.8     : '#444',
+			0.80001 : '#111',
+			1       : '#111'
+		},
+		container: d3.select('#scatterplot #heatmap-container')[0][0]
+	});
+
+	heatmap.setData({
+		min: 0,
+		max: 1,
+		data: data.data.map(function (d) {
+			return {x: xScatter(d.lng), y: yScatter(d.lat), value: 1};
+		})
+	});
+
+}
 
